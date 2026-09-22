4639d039d8080acb33a467622df8d685066c5b6d
HendraCahyaa <hendracahya345@gmail.com>
2026-09-22 09:23:11 +0700

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,45 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import "./index.css";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import BlogDetail from "./pages/BlogDetail";
+import CreateBlog from "./pages/CreateBlog";
+import { authLoader } from "./loaders/auth";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>hello</div>,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/home",
+    element: <HomePage />,
+  },
+  {
+    path: "/blogs/:objectId",
+    element: <BlogDetail />,
+  },
+  {
+    path: "/write",
+    element: <CreateBlog />,
+    loader: authLoader,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,77 @@
+import Loading from "@/components/Loading";
+import { Button } from "@/components/ui/button";
+import { axiosInstance } from "@/lib/axios";
+import { useLoginStore } from "@/stores/useLogin";
+import type { Blog } from "@/types/blogs";
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+
+function HomePage() {
+  const [Blogs, setBlogs] = useState<Blog[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const { user, logout } = useLoginStore();
+
+  const getBlogs = async () => {
+    try {
+      const { data } = await axiosInstance.get<any>("/data/Blogs");
+      if (Array.isArray(data)) {
+        setBlogs(data);
+      } else if (data && Array.isArray(data.results)) {
+        // Antisipasi jika SDK/API REST membungkusnya di dalam properti .results
+        setBlogs(data.results);
+      } else {
+        // Jika data bukan array (misal object eror dari server), amankan dengan array kosong
+        setBlogs([]);
+      }
+    } catch (error) {
+      console.log("error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getBlogs();
+  }, []);
+  return (
+    <div>
+      <div className="flex justify-center items-center h-24">
+        {user ? (
+          <div>
+            <h1>Welcome,{user.name}</h1>
+
+            <Button variant="destructive" onClick={logout}>
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <Link to="/write">
+            <Button>Login</Button>
+          </Link>
+        )}
+      </div>
+
+      {isLoading ? (
+        <div className="flex justify-center items-center h-100">
+          <Loading />
+        </div>
+      ) : (
+        <div className="flex flex-row gap-16 justify-center items-center">
+          {Blogs.map((blog) => {
+            return (
+              <Link key={blog.objectId} to={`/blogs/${blog.objectId}`}>
+                <div className="border-2 border-black p-8 ">
+                  <p className="text-lg font-bold">{blog.title}</p>
+                  <p>{blog.description}</p>
+                  <p>{blog.author}</p>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
+export default HomePage;
